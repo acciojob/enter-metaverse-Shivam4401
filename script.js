9337b58c76fc51f79cab5ce7f1d77e5ddffc7168
Shivam kumar <146973981+Shivam4401@users.noreply.github.com>
2025-03-02 17:15:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,6 @@
 let text = document.querySelector("#status");
 let btn = document.querySelector("#enterBtn");
 function changeP () {
-	text.innerHTML = "<h1>Enter the Metaverse</h1>"
+	text.innerHTML = "<h1>Entered Metaverse</h1>"
 }
 btn.addEventListener("click",changeP);
